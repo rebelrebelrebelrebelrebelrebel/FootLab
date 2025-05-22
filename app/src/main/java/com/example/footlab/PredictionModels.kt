9f2de00d1372fcc89ec31e8f59613cfa2d01ebda
RebelRivers <reginarios0696@gmail.com>
2025-05-22 10:26:21 -0600

    firebase sin storage

--- app/src/main/java/com/example/footlab/PredictionModels.kt
+++
@@ -1,7 +0,0 @@
-package com.example.footlab
-
-class PredictionModels {
-    data class PredictionRequest(val features: List<List<Double>>)
-    data class PredictionResponse(val predictions: List<Int>)
-
-}
